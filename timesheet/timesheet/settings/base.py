e913d6db801076de2c4fdb68f2471f1c16a49116
Vincent Alsteen <vincent.alsteen@gmail.com>
2014-05-05 23:22:47 +0200

    requirements.txt + bower

--- timesheet/timesheet/settings/base.py
+++ timesheet/timesheet/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ws4redis',
+    'djangobower',
 )
 
 WEBSOCKET_URL = '/ws/'
@@ -105,3 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = global_settings.STATICFILES_FINDERS + ('djangobower.finders.BowerFinder',)
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'components')
+
+BOWER_INSTALLED_APPS = (
+    'angular-pusher',
+)
