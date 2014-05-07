ff9cc0c7a43cd0bd0ac06be01dc4d3cad1c45975
Vincent Alsteen <vincent.alsteen@gmail.com>
2014-05-07 08:02:38 +0200

    Working test sending updated list

--- timesheet/main/static/js/controllers.js
+++ timesheet/main/static/js/controllers.js
@@ -4,11 +4,11 @@
 
 angular.module('TimeSheet.controllers', ['TimeSheet.django', 'TimeSheet.websocket'])
     .controller('MainView', ['$scope', 'WebSocket', function ($scope, WebSocket) {
-        $scope.lol = "I lol";
+        $scope.data = [];
         function receiveMessage(msg) {
             console.log(msg + "!");
             $scope.$apply(function () {
-                $scope.lol = msg;
+
             });
         }
         WebSocket.receive(receiveMessage);
--- timesheet/main/views.py
+++ timesheet/main/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import TemplateView
 from ws4redis.publisher import RedisPublisher
-
+import json
 
 class HomeView(TemplateView):
     template_name = 'index.html'
 
     def get(self, request, *args, **kwargs):
-        RedisPublisher(facility='foobar', broadcast=True).publish_message('Hello everybody')  # send a welcome message to everybody
+        people = [{'name': 'toto'}, {'name': 'riri'}]
+        RedisPublisher(facility='foobar', broadcast=True).publish_message(json.dumps(people))
         return super(HomeView, self).get(request, *args, **kwargs)
