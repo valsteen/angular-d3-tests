3dca712d8b0da2aac1ae0e677920972851b4d1a2
Vincent Alsteen <vincent.alsteen@gmail.com>
2014-05-11 21:49:21 +0200

    forgot 2 files

---
+++ timesheet/main/signals.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from rest_framework.utils import encoders
+from ws4redis.publisher import RedisPublisher
+from .rest import UserViewSet
+
+
+@receiver([post_save, post_delete], sender=User)
+def on_userlist_change(sender, **kwargs):
+    redis_publisher = RedisPublisher(facility='foobar', broadcast=True)
+
+    # a bit of a kludge, but doing so we easily send the list in the exact same format for convenience
+    user_view_set = UserViewSet()
+    user_list = user_view_set.serializer_class(User.objects.all(), many=True).data
+    json = encoders.JSONEncoder().encode(user_list)
+    redis_publisher.publish_message(json)
